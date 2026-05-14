cc1333db80861e0396d854d0866528959e35c627
holcomb52 <holcomb52@yahoo.com>
2026-05-14 10:42:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -748,39 +748,40 @@ with d:
         key="mopa_1"
     )
 
-        jobs.append({
-            "job_no": "1",
-            "concern": concern,
-            "cause": cause,
-            "correction": correction,
-            "tech_flagged_time": tech_flagged_time,
-            "time_allotted": time_allotted,
-            "claim_value": claim_value,
+    jobs.append({
+        "job_no": "1",
+        "concern": concern,
+        "cause": cause,
+        "correction": correction,
 
-            "oil_leak": oil_leak,
-            "oil_dye_billed": oil_dye_billed,
+         "tech_flagged_time": tech_flagged_time,
+         "time_allotted": time_allotted,
+         "claim_value": claim_value,
 
-            "battery_replacement": battery_replacement,
-            "battery_test_slip": battery_test_slip,
-    
-            "sublet_repair": sublet_repair,
-            "sublet_vin": sublet_vin,
-            "sublet_mileage": sublet_mileage,
-            "sublet_notes": sublet_notes,
+         "oil_leak": oil_leak,
+         "oil_dye_billed": oil_dye_billed,
 
-            "rental_involved": rental_involved,
-            "rental_days": rental_days,
-            "manager_signed_rental": manager_signed_rental,
+         "battery_replacement": battery_replacement,
+         "battery_test_slip": battery_test_slip,
 
-            "warranty_add_on": warranty_add_on,
-            "manager_approval": manager_approval,
+        "sublet_repair": sublet_repair,
+        "sublet_vin": sublet_vin,
+        "sublet_mileage": sublet_mileage,
+        "sublet_notes": sublet_notes,
 
-            "ac_repair": ac_repair,
-            "ac_evac_slip": ac_evac_slip,
+        "rental_involved": rental_involved,
+        "rental_days": rental_days,
+        "manager_signed_rental": manager_signed_rental,
 
-            "parts_warranty": parts_warranty,
-            "mopa_original_ro": mopa_original_ro
-})
+        "warranty_add_on": warranty_add_on,
+         "manager_approval": manager_approval,
+
+         "ac_repair": ac_repair,
+        "ac_evac_slip": ac_evac_slip,
+
+        "parts_warranty": parts_warranty,
+        "mopa_original_ro": mopa_original_ro
+        })
 
     st.divider()
 
